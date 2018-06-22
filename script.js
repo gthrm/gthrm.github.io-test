e3f5dcd7aa2444761cd860fc44ade5d6daa4da6f
cdROma <mrv24ru@yandex.ru>
2018-06-23 01:06:31 +0300

    ver. 1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (
       $("body").append("<div class='circle' data-x='"+x+"' data-y='"+y+"' style='top:"+y+"px;left:"+x+"px'></div>");
     }
   }
-      $(window).on("touchmove", function(document) {
+      $(window).on("mousemove", function(document) {
         $(".circle").each(function(i){
           let x0 =+ $(".circle:eq("+i+")").data("x")+$(".circle:eq("+i+")").width()/2;
           let y0 =+ $(".circle:eq("+i+")").data("y")+$(".circle:eq("+i+")").width()/2;
